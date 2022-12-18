bd39c3d807e6543f085a7f4c0bdf78d8a22d17e6
jeffhu <jeff1hu@gmail.com>
2022-12-18 09:10:26 -0500

    added corner checking

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,152 @@
 import React, {MouseEventHandler, useEffect, useMemo, useRef, useState} from 'react';
 import Box from "./Box";
 import './App.css';
+import {Simulate} from "react-dom/test-utils";
+import change = Simulate.change;
 
 const throttle = require('lodash.throttle');
 
+interface Vector {
+    x: number,
+    y: number
+}
+
 function App() {
     const boxRef = useRef();
-    const [mousePosition, setMousePosition] = useState([0,0]); // using array for micro optimization
+    const [mousePosition, setMousePosition] = useState({x: 0,y: 0});
+    const stateRef = useRef(mousePosition);
+    stateRef.current = mousePosition;
+
+    const [a1, seta1] = useState({x: 0, y: 0})
+    const seta1ref = useRef(seta1)
+    seta1ref.current = seta1
+
+    const [m, setm] = useState({x:0, y:0})
+    const mref = useRef(setm)
+    mref.current = setm
+
+    const [c1, setc1] = useState({x: 0, y: 0})
+    const setc1ref = useRef(setc1)
+    setc1ref.current = setc1
+
+    const [c2, setc2] = useState({x: 0, y: 0})
+    const setc2ref = useRef(setc2)
+    setc2ref.current = setc2
+
+    const dot = (u: Vector, v:Vector) => {
+        return u.x * v.x + u.y * v.y
+    }
+
+    const unit = (u: Vector) => {
+        const mag = Math.sqrt(u.x* u.x + u.y * u.y)
+        return {x: u.x/mag, y: u.y/mag}
+    }
+
+    const sub = (u:Vector, v:Vector) => {
+        return {x: u.x - v.x, y: u.y - v.y}
+    }
+
+    const add = (u: Vector, v:Vector) => {
+        return {x: u.x + v.x, y: u.y + v.y}
+    }
 
     const mouseMoveHandler = useMemo(() => throttle((e: React.MouseEvent) => {
-        setMousePosition([e.clientX, e.clientY]);
+        setMousePosition({x: e.clientX, y: e.clientY});
     }, 100), [])
 
-    const positionsHandler = (positions: {x:number, y:number}[]) => {
+    const checkAngle = (angle1:number, angle2: number) => {
+        return (angle1 <= 3.14159 / 2 && angle2 > 3.14159/2) || (angle1 > 3.14159 / 2 && angle2 <= 3.14159/2)
+    }
 
+    const positionsHandler = ({a,b,c,d,e}: {a: Vector, b: Vector, c: Vector, d: Vector, e:Vector}) => {
+        //c to a on a
+        let changeCorner = setc1ref.current
+
+        const ca = unit(sub(a,c)) //unit a-c
+        const ma = unit(sub(a, stateRef.current)) // unit a - mousePosition
+        const ba = unit(sub(a, b))
+
+        const angletl = Math.acos(dot(ca, ma));
+        const angletl2 = Math.acos(dot(ba, ma));
+
+        if (checkAngle(angletl, angletl2)) {
+            changeCorner(a);
+            changeCorner = setc2ref.current
+        }
+
+        const ab = unit(sub(b,a))
+        const mb = unit(sub(b, stateRef.current))
+        const db = unit(sub(b,d))
+
+        const angletr = Math.acos(dot(ab,mb))
+        const angletr2 = Math.acos(dot(db,mb))
+
+        if (checkAngle(angletr, angletr2)) {
+            changeCorner(b)
+            changeCorner = setc2ref.current
+        }
+
+        const ac = unit(sub(c,a))
+        const mc = unit(sub(c, stateRef.current))
+        const dc = unit(sub(c,d))
+
+        const anglebl = Math.acos(dot(ac,mc))
+        const anglebl2 = Math.acos(dot(dc,mc))
+
+        if (checkAngle(anglebl, anglebl2)) {
+            changeCorner(c)
+            changeCorner = setc2ref.current
+        }
+
+        const bd = unit(sub(d,b))
+        const md = unit(sub(d, stateRef.current))
+        const cd = unit(sub(d, c))
+
+        const anglebr = Math.acos(dot(bd,md))
+        const anglebr2 = Math.acos(dot(cd,md))
+
+        if (checkAngle(anglebr, anglebr2)) {
+            changeCorner(d)
+        }
+
+        // const temp3 = unit(add(sub(a, b), a))  // unit a-b + a
+
+        // seta1ref.current(add({x: ca.x * 50, y: ca.y * 50}, a))
+        // mref.current(add({x: ma.x * 50, y: ma.y * 50}, a))
+        // setc2ref.current(add({x: ba.x * 50, y: ba.y * 50}, a))
+        // console.log(Math.acos(dot(ca, ma))*180/3.14159)
+
+        // console.log(temp)
+        // console.log(a,b,c,d)
+        // console.log(stateRef.current)
     }
 
   return (
     <div className="App" onMouseMove={mouseMoveHandler}>
         <div className="box-holder">
             <Box sendPos = {positionsHandler}/>
+        </div>
+        <div className="test" style={{
+            top: a1.y,
+            left: a1.x
+        }}></div>
+
+        <div className="test" style={{
+            top: m.y,
+            left: m.x
+        }}>
+        </div>
+
+        <div className="corner" style={{
+            top: c1.y,
+            left: c1.x
+        }}>
+        </div>
+
+        <div className="corner" style={{
+            top: c2.y,
+            left: c2.x
+        }}>
         </div>
     </div>
   );
--- src/Box.tsx
+++ src/Box.tsx
@@ -1,8 +1,12 @@
-import React, {useEffect, useRef} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import './Box.css';
 
+interface Vector {
+    x: number,
+    y: number
+}
 interface BoxProps {
-    sendPos: (positions: {x: number, y:number}[]) => void;
+    sendPos: (positions: {a: Vector, b: Vector, c: Vector, d: Vector, e:Vector}) => void;
 }
 
 function Box({sendPos}: BoxProps) {
@@ -10,9 +14,19 @@ function Box({sendPos}: BoxProps) {
     const topRightRef = useRef<HTMLDivElement>(null);
     const bottomLeftRef = useRef<HTMLDivElement>(null);
     const bottomRightRef = useRef<HTMLDivElement>(null);
+    const boxRef = useRef<HTMLDivElement>(null)
+    const [center, setCenter] = useState({x: 0, y:0});
 
     useEffect(() => {
-        setInterval(() => {
+        if (boxRef.current == null) {
+            return
+        }
+        const bounding = boxRef.current.getBoundingClientRect()
+        setCenter({x: (bounding.right - bounding.left)/2 + bounding.left, y: (bounding.bottom - bounding.top)/2 + bounding.top})
+    }, [])
+
+    useEffect(() => {
+        const interval = setInterval(() => {
             if (topLeftRef.current == null || topRightRef.current == null ||
                 bottomLeftRef.current == null || bottomRightRef.current == null) {
                 return;
@@ -23,19 +37,24 @@ function Box({sendPos}: BoxProps) {
             const bottomLeftRefBounding = bottomLeftRef.current.getBoundingClientRect();
             const bottomRightRefBounding = bottomRightRef.current.getBoundingClientRect();
 
-            const positionArray = [
-                {x: topLeftRefBounding.x, y: topLeftRefBounding.y},
-                {x: topRightRefBounding.x, y:topRightRefBounding.y},
-                {x: bottomLeftRefBounding.x, y:bottomLeftRefBounding.y},
-                {x: bottomRightRefBounding.x, y: bottomRightRefBounding.y}
-            ]
+            const positionArray = {
+                a: {x: topLeftRefBounding.x, y: topLeftRefBounding.y},
+                b: {x: topRightRefBounding.x, y:topRightRefBounding.y},
+                c: {x: bottomLeftRefBounding.x, y:bottomLeftRefBounding.y},
+                d: {x: bottomRightRefBounding.x, y: bottomRightRefBounding.y},
+                e: center
+            }
 
             sendPos(positionArray)
-        }, 100)
-    })
+        }, 100);
+
+        return () => {
+            clearTimeout(interval);
+        }
+    }, [])
 
     return (
-        <div className="box">
+        <div className="box" ref={boxRef}>
             <div className="top-left-corner" ref={topLeftRef}></div>
             <div className="top-right-corner" ref={topRightRef}></div>
             <div className="bottom-left-corner" ref={bottomLeftRef}></div>
