85f076a10b9262361c2b4ae3a317155d225432bb
Jeff Hu <j.hu@mail.utoronto.ca>
2022-12-22 23:56:37 -0700

    refactor

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './App.css'
+import Shadow from "./Shadow";
+
+export default function App() {
+    return (
+        <div className={'App'}>
+            <Shadow/>
+        </div>
+    )
+}
